4ab3e064868124146cb077fcbd3825369eaa7920
Abhi <abhigya.sa@gmail.com>
2020-07-24 22:42:07 -0400

    todo app with react and django rest

---
+++ todo_react_django/frontend/build/precache-manifest.fe15b309e467f5dcc22ef48497cc1fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537ce724cc4cef1304a83bf906438a9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a43eb43c16d0f3e9a3d",
+    "url": "/static/css/main.37e759d6.chunk.css"
+  },
+  {
+    "revision": "7f9f17edf8828e3a02a8",
+    "url": "/static/js/2.e2029a90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2029a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a43eb43c16d0f3e9a3d",
+    "url": "/static/js/main.68af9eef.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
